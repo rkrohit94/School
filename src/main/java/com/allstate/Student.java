c8f81224b3c972d70a930550dd04657cbf5e1559
lavanyareddygowlla <lredg@allstate.com>
2017-01-30 09:35:27 +0530

    running setup

---
+++ src/main/java/com/allstate/Student.java
@@ -0,0 +1,55 @@
+package com.allstate;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+@Entity
+@Table(name = "students")
+public class Student {
+    private int id;
+    private int version;
+    private String email;
+    private Date created;
+    private Date modified;
+
+    @Id
+    @GeneratedValue
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Version
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    @CreationTimestamp
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    @UpdateTimestamp
+    public Date getModified() {
+        return modified;
+    }
+
+    public void setModified(Date modified) {
+        this.modified = modified;
+    }
+}
